cb9db563e51ff4fc82524df97358f040bc35593d
Hemanshu Marwadi <Hemanshu.Marwadi@act.local>
2026-03-15 09:09:23 +0530

    Extract data from html file using lxml library and also used of X- PATH and Connect Database and Data push on table

---
+++ db_config.py
@@ -0,0 +1,45 @@
+import mysql.connector
+
+
+def make_connection():
+    conn = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='actowiz',
+        database='bk_stores'
+    )
+    return conn
+
+def create_table(table_name: str):
+
+    create_query = f'''
+    CREATE TABLE IF NOT EXISTS {table_name}(
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        brand_name VARCHAR(200),
+        address TEXT,
+        locality VARCHAR(200),
+        city VARCHAR(100),
+        pincode VARCHAR(20),
+        phone_number VARCHAR(50),
+        timing VARCHAR(100),
+        store_url TEXT,
+        map_url TEXT
+    )
+    '''
+
+    conn = make_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(create_query)
+
+    conn.commit()
+    conn.close()
+
+def insert_into_db(table_name: str, data: dict):
+    cols = ",".join(list(data.keys()))
+    vals = "".join([len(data.keys()) * '%s,']).strip(',')
+    q = f"""INSERT INTO {table_name} ({cols}) VALUES ({vals})"""
+    conn = make_connection()
+    cursor = conn.cursor()
+    cursor.execute(q, tuple(data.values()))
+    conn.commit()
---
+++ extract.py
@@ -0,0 +1,38 @@
+from lxml import html
+from utils import read_htmlFile
+from db_config import create_table , insert_into_db
+
+base_path = r'C:\Users\hemanshu.marwadi\Desktop\Himanshu Marwadi\x-path\bk_store\bk_store.html'
+TABLE_NAME = 'burger_king_stores'
+
+def parsel(html_data):
+    burger_king = []
+    tree = html.fromstring(html_data)
+    store_info = tree.xpath('//div[@class = "store-info-box"]')
+    for information in store_info:
+        bk_stores = {}
+        bk_stores['brand_name'] = information.xpath('normalize-space(string(.//li[@class = "outlet-name"]//a))')
+        bk_stores['address'] = information.xpath('normalize-space(string(.//li[@class = "outlet-address"]//span/text()))')
+        bk_stores['locality'] = information.xpath('normalize-space(string(.//div[@class="info-text"]//span[2]))')
+        bk_stores['city'] = information.xpath('normalize-space(string(.//span[@class="merge-in-next"]/span[1]))')
+        bk_stores['pincode'] = information.xpath('normalize-space(.//span[@class="merge-in-next"]//span[3])')
+        bk_stores['phone_number'] = information.xpath('normalize-space(.//li[@class = "outlet-phone"]//a)')
+        bk_stores['timing'] = information.xpath('normalize-space(.//li[@class = "outlet-timings"]//div[@class = "info-text"]//span)')
+        bk_stores['store_url'] = information.xpath('normalize-space(.//a[@class = "btn btn-website"]//@href)')
+        bk_stores['map_url'] = information.xpath('normalize-space(.//a[@class = "btn btn-map"]//@href)')
+        burger_king.append(bk_stores)
+    
+    return burger_king
+        
+    
+
+def main():
+    create_table(table_name=TABLE_NAME)
+    raw = read_htmlFile(base_path)
+    pars_data = parsel(raw)
+    for row in pars_data:
+        insert_into_db(table_name=TABLE_NAME, data=row)
+    
+    
+if __name__ == '__main__':
+    main()
